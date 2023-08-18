76470f2a94664e3a9612b62574d490a312221c1f
xavierrice <xavier.rice@gmail.com>
2023-08-18 12:39:51 -0400

    fetched from main api

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+const test = console.log
 import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './App.css'
 
-
-
+const secret = import.meta.env.VITE_REACT_APP_API_KEY
 
 
 function App() {
-
+  
+  const [ allVideos, setAllVideos] = useState([])
+  
+  useEffect(() => {
+  
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?key=${secret}`)
+        .then(r => r.json())
+        .then(arr => setAllVideos(arr))
+        .catch(err => console.log(err))
+  },[])
+  
+  test(allVideos)
 
   return (
     
