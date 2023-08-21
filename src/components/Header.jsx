0b6c14c0840451871f3ed7f0ad0425495e92768c
xavierrice <xavier.rice@gmail.com>
2023-08-21 13:57:10 -0400

    add Randy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,27 +5,25 @@ const Header = () => {
     return (
 
         <header>
- 
 
-  
+            <div className="navbar bg-light my-2">
+                <nav>
+                    <Link to="/" >
+                        <h3>Home</h3>
+                    </Link>
 
-        <div className="navbar bg-light my-2">
-            <nav>
-                <Link to="/" >
-                    <h3>Home</h3>
-                </Link>
+                    <Link to="/about" >
+                        <h3>About Us</h3>
+                    </Link>
 
-                <Link to="/about" >
-                    <h3>About Us</h3>
-                </Link>
-
-                <Link to="/search" >
-                    <h3>Search</h3>
-                </Link>
-            </nav>
-        </div>
-</header>
+                    <Link to="/search" >
+                        <h3>Search</h3>
+                    </Link>
+                </nav>
+            </div>
+            
+        </header>
     )
 }
-    
+
 export default Header;
