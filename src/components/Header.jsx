51ba558b8cf4b8af293eeff74625953516b114b5
xavierrice <xavier.rice@gmail.com>
2023-08-24 13:05:49 -0400

    changed png name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
             <div className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container">
                     <div className="img-style">
-                        <img src="./src/assets/8F0EB4BC-DE25-4B61-A679-C2BC48E906CC.png" alt="image of a red figure watching the tube" width="310px" height="120" />
+                        <img src="./src/assets/youtube.png" alt="image of a red figure watching the tube" width="310px" height="120" />
                     </div>
                     <div className="video-style" >
                         <img src="./src/assets/74H4.gif" alt="animation of children dancing" width="220px" height="160px" />
