c3225b260679aa6f9567ea81cc861df53d7b58f4
xavierrice <xavier.rice@gmail.com>
2023-08-24 13:01:03 -0400

    done and done

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
 
   useEffect(() => {
 
-    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${searchKey}+song&type=video&maxResults=8&key=${secret}`)
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${searchKey}+song&type=video&maxResults=8&key=${xsecret}`)
       .then(r => r.json())
       .then(data => setAllVideos(data))
       .catch(err => console.log(err))
@@ -47,36 +47,3 @@ function App() {
 export default App;
 
 
-//COMPONENTS//
-/*
- 
- 
- 
-  < div className = "search-bar-container" >
-
-        <SearchBar setResults={setResults} />     // I'm moved this out of the app because it wasn't responding when i loaded it and there was no seachBar made so i made one, We're you thinking "searchHistory?". Could you guide me through it? Looks like ur working with a userObj? can u create one if so?
-        <SearchResultsList results={results}/>
-</div >
- 
-<SearchBar searchKey={searchKey} setSearchKey={setSearchKey}/>
-<VideoMaker allVideos={allVideos} searchKey={searchKey}/>
-<Footer/>
-<NabBar/>
- 
- <Route path=':videoId' element={<VideoPage/>}/>
-
-       <Routes>
-        <Route path='/' element={<h1>Home</h1>} />   this should lead to: header, searchbar, videmaker and footer
-        <Route path='/about' element={ <AboutPage/>} />  <AboutPage/> 
-        <Route path='/search' element={<h1>Search</h1>} />  this should: header, searchBar and footer  
-        <Route path=`/video/${video.id.videoId}` element={ VideoPage video={video}}
-      </Routes>
-
-
-
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/about' element={ <AboutPage/>} />
-        <Route path=':videoId' element={<VideoPage />} />
-      </Routes>
-*/
--- src/components/VideoPage.jsx
+++ src/components/VideoPage.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { GoThumbsdown, GoThumbsup } from "react-icons/go";
 import Footer from "./Footer";
+import Header from "./Header";
 
-
-const VideoPage = (  ) => {
+const VideoPage = () => {
 
   function LikeNDislikes() {
 
@@ -12,19 +12,19 @@ const VideoPage = (  ) => {
     const [dislike, setDislike] = useState(0)
 
     return (
-        <>
-            <div className="like-style" key="like-button">
-                <button onClick={() => setLike((like) => like + 1)} > <GoThumbsup id="thumbs-up" /> </button >
-            </div >
-            <div className="dislike-style" key="dislike-button">
-                <button onClick={() => setDislike((dislike) => dislike + 1)} > <GoThumbsdown id="thumbs-down" /> </button>
-            </div>
-             <div> <h4>Likes:{like}Disikes:{dislike}</h4> </div>
-        </>
+      <>
+        <div className="like-style" key="like-button">
+          <button onClick={() => setLike((like) => like + 1)} > <GoThumbsup id="thumbs-up" /> </button >
+        </div >
+        <div className="dislike-style" key="dislike-button">
+          <button onClick={() => setDislike((dislike) => dislike + 1)} > <GoThumbsdown id="thumbs-down" /> </button>
+        </div>
+        <div> <h4>Likes:{like}Disikes:{dislike}</h4> </div>
+      </>
     )
-};
+  };
 
-let { videoId }  = useParams();
+  let { videoId } = useParams();
 
 
   const [comment, setComment] = useState("");
@@ -33,7 +33,7 @@ let { videoId }  = useParams();
 
 
   function handleComment(e) {
-       
+
     setComment(e.target.value)
   };
 
@@ -45,7 +45,7 @@ let { videoId }  = useParams();
   function handleSubmit(e) {
     e.preventDefault();
 
-    if(name === ""){
+    if (name === "") {
       alert("You must enter a name")
       setName(" ");
       return null;
@@ -55,71 +55,75 @@ let { videoId }  = useParams();
       return null;
     } else {
 
-    const newNote = {
-      commentor: name,
-      comment: comment
+      const newNote = {
+        commentor: name,
+        comment: comment
+      }
+      setNote([...note, newNote])
+      setComment(" ");
+      setName(" ")
+
     }
-    setNote([...note, newNote])
-    setComment(" ");
-    setName(" ")
+  };
 
-  }};
-
-  const renderComments = note.map( (note, i) => {
+  const renderComments = note.map((note, i) => {
     return (
-        <h6 key={i}>
-         <strong>{note.commentor}:{"   "}</strong><em>{note.comment}</em>
-        </h6>
+      <h6 key={i}>
+        <strong>{note.commentor}:{"   "}</strong><em>{note.comment}</em>
+      </h6>
     )
-  }) 
+  })
 
   return (
-      <>
-    <div className="video-responsive"  key={videoId}>
-      <div className="player" >
-        <iframe className="youtube-player"
-          type="video/mp4"
-          width="853"
-          height="480"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture "
-          allowFullScreen
-          title="Embedded youtube"
-          src={`https://www.youtube.com/embed/${videoId}`}
+    <>
+      <Header />
+      <div className="video-responsive mt-4" key={videoId}>
+        <div className="player" >
+          <iframe className="youtube-player"
+            type="video/mp4"
+            width="853"
+            height="480"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture "
+            allowFullScreen
+            title="Embedded youtube"
+            src={`https://www.youtube.com/embed/${videoId}`}
           />
+        </div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">
+            Name
+            <input
+              style={{ width: '29rem' }}
+              id="name"
+              name="name"
+              type="text"
+              value={name}
+              onChange={handleName}
+              placeholder="Your name..."
+            />
+          </label>
+          <label htmlFor="comment">
+            Comment
+            <input
+              id="comment"
+              style={{ width: '44rem', height: '9rem', backgroundColor: "white", marginTop: "10px" }}
+              name="comment"
+              type="text"
+              value={comment}
+              onChange={handleComment}
+              placeholder="Add a brief message..."
+            />
+          </label>
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </form>
+        <div className="box return" style={{ justifyContent: "left" }}>
+          {renderComments}
+        </div>
+        <div className="mt-3">
+          <LikeNDislikes />
+        </div>
       </div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">
-          Name
-          <input
-            style={{ width: '29rem' }}
-            id= "name"
-            name="name"
-            type="text"
-            value={name}
-            onChange={handleName}
-            placeholder="Your name..."
-          />
-        </label>
-        <label htmlFor="comment">
-          Comment
-          <input
-            id="comment"
-            style={{ width: '44rem', height: '9rem', backgroundColor: "white", marginTop: "10px" }}
-            name="comment"
-            type="text"
-            value={comment}
-            onChange={handleComment}
-            placeholder="Add a brief message..."
-          />
-        </label>
-        <button type="submit" className="btn btn-secondary">Submit</button>
-      </form>
-      <div className="box return" style={{ justifyContent:"left" }}>
-        {renderComments}
-      </div>
-      <LikeNDislikes/>
-    </div>
-    <Footer/>
+
     </>
   )
 };
